f21e1a82382f7a02a30c534ac8eb268b9f593ac1
GhostFoxGodxShootboy621 <idkbroxxxrrr@gmail.com>
2025-03-23 16:49:46 +0530

    Setup

---
+++ index.py
@@ -0,0 +1,9 @@
+Number = int(input("Enter a number: "))
+print(Number) # prints enter no.
+print(Number + 10) # prints enter no. + 10
+print(Number - 10) # prints enter no. - 10
+print(Number * 10) # prints enter no. * 10
+print(Number / 10) # prints enter no. / 10
+print(Number % 10) # prints enter no. % 10
+print(Number ** 10) # prints enter no. ** 10
+print(Number // 10) # prints enter no. // 10
